1774649a249e48c4e7311c5b41cb3e640bb6445b
GladysKigen <gladys.kigen@student.moringa.com>
2023-03-24 22:31:02 +0300

    completed nested routing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+   # items = Item.all
+   # render json: items, include: :user
+   if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+   else
     items = Item.all
-    render json: items, include: :user
+   end
+   render json: items, except: [:created_at, :updated_at]# include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, except: [:created_at, :updated_at]
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, except: [:created_at, :updated_at], status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: {error: "Item not found"}, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
+    #render json: user
     render json: user, include: :items
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "user not found"}, status: :not found
   end
 
 end
